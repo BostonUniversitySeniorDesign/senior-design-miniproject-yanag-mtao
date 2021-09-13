ce74bce300448d8243140bb3de73a37cabd976f7
Yana Galina <yanag@bu.edu>
2021-09-13 18:36:26 -0400

    Added delete ingredient button

--- mobile-app/NutritionApp/screens/newRecipeScreen.js
+++ mobile-app/NutritionApp/screens/newRecipeScreen.js
@@ -9,52 +9,68 @@ import 'firebase/firestore';
 
 import AddFoodButton from "../components/addFoodButton";
 
-const dbh = firebase.firestore();
 
-const recipes = [];
-const renderRow = (datum, i) => {
-    return (
-        <View key={i} style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row' }}>
-            <View style={{ flex: 1, alignSelf: 'stretch' }}>
-              <Text>
-                {datum.name}
-              </Text>
-            </View>
-            <View style={{ flex: 1, alignSelf: 'stretch' }}>
-              <Text>
-                {datum.servings}
-              </Text>
-            </View>
-            <View style={{ flex: 1, alignSelf: 'stretch' }}>
-              <Text>
-                {datum["calories per serving"]}
-              </Text>
-            </View>
-        </View>
-    );
-};
 
-const getNutritionDataUsingAsyncAwaitGetCall = async (barcode) => {
-    try {
-      const response = await axios
-        .post('https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY', {
-           query: barcode,
-           dataType: ["Branded"],
-           userId: 1,
-         });
-      alert(JSON.stringify(response.data));
-    } catch (error) {
-      alert(error.message);
-    }
+
+const Ingredients = (props) => {
+  const renderRow = (datum, i) => {
+      return (
+          <View key={i} style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row' }}>
+              <View style={{ flex: 1, alignSelf: 'stretch' }}>
+                <Text>
+                  {datum.name}
+                </Text>
+              </View>
+              <View style={{ flex: 1, alignSelf: 'stretch' }}>
+                <Text>
+                  {datum.servings}
+                </Text>
+              </View>
+              <View style={{ flex: 1, alignSelf: 'stretch' }}>
+                <Text>
+                  {datum["calories per serving"]}
+                </Text>
+              </View>
+              <View style={{ flex: 1, alignSelf: 'stretch' }}>
+                <Text>
+                  {datum["calories per serving"]}
+                </Text>
+              </View>
+              <Button
+                title="del"
+                onPress={() => props.deleteIngredient(i)}
+              />
+          </View>
+      );
   };
 
 
-const Ingredients = () => {
-  const [ loading, setLoading ] = useState(true);
-  const [ recipe, setRecipe ]   = useState([]);
+  if (props.loading) {
+    return <Text> Loading ingredients...</Text>
+  }
 
+  return (
+    <>
+    <Text>{props.recipe.name}</Text>
+    {
+      props.recipe.ingredients.map((datum, i) => { // This will render a row for each data element.
+        return renderRow(datum, i);
+      })
+    }
+    </>
+  );
+};
+
+
+export default function NewRecipeScreen({ navigation, route, props}) {
+  const [ loading, setLoading ]             = useState(true);
+  const [ recipe, setRecipe ]               = useState({});
+  const [ totalCalories, setTotalCalories ] = useState(0);
+
+  const dbh = firebase.firestore();
   const ref = dbh.collection('recipes').doc("w4NgFrIpJ59ccPJv3lDw");
 
+  // update ingredients from database as needed
   useEffect(() => {
     ref
       .onSnapshot((doc) => {
@@ -69,41 +85,97 @@ const Ingredients = () => {
 
   }, []);
 
-  if (loading) {
-    return null;
-  }
-
-  return (
-    <>
-    <Text>{recipe.name}</Text>
-    {
-      recipe.ingredients.map((datum, i) => { // This will render a row for each data element.
-        return renderRow(datum, i);
-      })
+  // Update total calories every time ingredients get updated
+  useEffect(() => {
+    if (recipe.ingredients) {
+      var totalCals = 0;
+        recipe.ingredients.forEach(el => {totalCals += el["calories per serving"]*el.servings});
+        setTotalCalories(totalCals);
     }
-    </>
-  );
-};
 
+  }, [recipe]);
 
-export default function NewRecipeScreen({ navigation, route }) {
-  const recipes = useState([]);
-
+  // Handle getting barcode data from barcode scanner
   React.useEffect(() => {
-        if (route.params?.barCodeData) {
-          alert(`Bar code with data ${route.params?.barCodeData} has been scanned!`);
-          getNutritionDataUsingAsyncAwaitGetCall(route.params?.barCodeData);
+    if (route.params?.barCodeData) {
+      // Query USDA API
+      getNutritionDataUsingAsyncAwaitGetCall(route.params?.barCodeData);
 
+    }
+  }, [route.params?.barCodeData]);
+
+
+  const getNutritionDataUsingAsyncAwaitGetCall = async (barcode) => {
+      // query USDA API
+      try {
+        const response = await axios
+          .post('https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY', {
+             query: barcode,
+             dataType: ["Branded"],
+             userId: 1,
+           });
+        console.log(JSON.stringify(response.data));
+        data = response.data;
+        if (data.totalHits == 0) {
+          alert(`Unable to find food with barcode ${barcode}`);
         }
-      }, [route.params?.barCodeData]);
+        else {
+          food = data.foods[0];
+          var cals = 0;
+          for (let nutrient of food.foodNutrients) {
+            console.log(nutrient);
+            if (nutrient.nutrientName == "Energy") {
+              cals = nutrient.value;
+            }
+          }
+          ingredient = {
+            'name' : food.lowercaseDescription,
+            'servings': 1,
+            "calories per serving": cals,
+          };
+
+          // update current state to show new ingredient
+          setRecipe({...recipe, "ingredients": [...recipe.ingredients, ingredient]});
+        }
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+
+  const saveRecipe = async () => {
+    try {
+      ref.update(recipe);
+    }
+    catch (error) {
+      alert(error.message);
+    }
+
+    alert("Successfully saved recipe!");
+
+
+  };
+
+  const deleteIngredient = (idx) => {
+    // update current state to show new ingredient
+    console.log(`deleting ingredient ${idx}`);
+    var res = recipe.ingredients.filter((e, i) => {
+      return (i != idx);
+    });
+
+    setRecipe({
+      ...recipe,
+      "ingredients": res,
+    });
+  }
 
   return (
     <View style={styles.container}>
           <Text>Creating New Recipe</Text>
-          <Ingredients/>
+          <Ingredients recipe={recipe} loading={loading} deleteIngredient={deleteIngredient}/>
+          <Text>Total Calories: {totalCalories}</Text>
           <Button
             title="Save"
-            onPress={() =>  navigation.goBack()}
+            onPress={saveRecipe}
           />
           <AddFoodButton onPress={() => navigation.navigate('BarCode')}/>
     </View>
